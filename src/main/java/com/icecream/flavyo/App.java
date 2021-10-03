516fc2bbc5491f70061f83f673c0cb90a68e0f3a
akriti <akriti.sharma53@gmail.com>
2021-10-03 14:48:16 +0530

    base commit

---
+++ src/main/java/com/icecream/flavyo/App.java
@@ -0,0 +1,13 @@
+package com.icecream.flavyo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
